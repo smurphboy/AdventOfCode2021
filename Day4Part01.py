bc8017f3d99a15945fb47aa4d0ff7dd5cf700e6a
Simon Murphy <simon.murphy@gmail.com>
2021-12-10 16:32:01 +0000

    Day04Part01 reading in rows correctly into dict of lists

--- Day4Part01.py
+++ Day4Part01.py
@@ -3,17 +3,19 @@
 def main():
     with open('input/dayfourinput.txt', 'rt') as file:
         lines = file.readlines()
-
+        cardlines = {}
         for idx, line in enumerate(lines):
+            card = int((idx-1)/6)
+            row = (idx-1) % 6
             if idx == 0:
                 bingonumbers = line
+            if idx == 1:
+                pass
             else:
-                if (idx-1) % 6 == 1:
-                    print (idx, 'line 1 of card', int(idx / 6), line)
-
-        print (bingonumbers)
-        print (len(lines))
-
+                cardlines.update({str(card)+'r'+str(row) : [line.split()]})
+#        print (bingonumbers)
+#        print (len(lines))
+        print(cardlines)
 
 if __name__ == '__main__':
     main()
